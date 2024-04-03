ce5c6b3334dd84b4cc25c574a75a4d59f504029c
Ambreen Sheikh <ambreensheikh900@gmail.com>
2024-04-03 23:15:02 +0500

    first commit

---
+++ 88-roundingTo-Nearest/app.js
@@ -0,0 +1,6 @@
+//make a function rounds a decimal number to the whole number
+function roundToNearst(num) {
+    return Math.round(num);
+}
+console.log(roundToNearst(6.8));
+export {};
---
+++ 90-checkingValue-NaN/app.js
@@ -0,0 +1,7 @@
+// function check if value is not a number
+function valueNaN(valueType) {
+    return isNaN(valueType);
+}
+console.log(valueNaN("Hello!"));
+console.log(valueNaN("456"));
+export {};
